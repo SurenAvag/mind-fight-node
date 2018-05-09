cd7ecb308a23622e3ac00b2a0730b03c86e6c0e8
SurenAvag <s404747@gmail.com>
2018-05-09 19:29:19 +0400

    sdg

--- app/main.js
+++ app/main.js
@@ -17,9 +17,50 @@ redisClient.on("message", function(channel, data) {
 });
 
 io.use(function(socket, next) {
-	return next();
-	// next(new Error('Authentication error'));
+    let token = socket.handshake.query && socket.handshake.query.accessToken;
+    if(!token){
+        next(new Error('Authentication error'));
+    }
+    var options = {
+        uri: `${process.env.REST_CLIENT}me`,
+        headers: {
+            'User-Agent': 'Request-Promise',
+            'Accept': 'application/json',
+            'Authorization': `Bearer ${token}`
+        },
+        rejectUnauthorized: false,
+        json: true // Automatically parses the JSON string in the response
+    };
+    rp(options)
+        .then((res) => {
+            socket.user = res;
+            next();
+        })
+        .catch((err) => {
+            console.log('Handshake query rejected', err);
+            next(new Error(err));
+        });
 });
 
-io.sockets.on('connection', function (socket) {
+io.sockets.on('connection', (socket) => {
+    var collections = socket.user.collection_ids;
+    console.log(socket.user.id);
+    // collections = collections.map((val) => val.id);
+    socket.join(`user::${socket.user.id}`);
+    if(socket.user){
+        // Join to room named collection::Id
+        collections.forEach((val) => {
+            socket.join(`collection::${val}`);
+        });
+
+    }
+    socket.on('disconnect', (data) => {
+        // disconnected from rooms named collection::Id
+        collections.forEach((val) => {
+            console.log(`leaving collection::${val}`)
+            socket.leave(`collection::${val}`);
+        });
+        socket.leave(`user::${socket.user.id}`);
+
+    })
 });
