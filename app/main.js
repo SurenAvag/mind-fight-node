b056d821f10abf0736129020d0c20c892066491e
SurenAvagyanG <suren@fifth-llc.com>
2018-05-12 21:17:32 +0400

    init

--- app/main.js
+++ app/main.js
@@ -1,20 +1,26 @@
 var	rp = require('request-promise');
+redisClient.subscribe('game-created');
 
-redisClient.subscribe('collection-created');
-redisClient.subscribe('collection-updated');
-redisClient.subscribe('collection-deleted');
+let onlineUsers = [];
 
 redisClient.on("message", function(channel, data) {
 	var dataJson = JSON.parse(data);
 	if(dataJson && dataJson.data){
-		if(channel === 'collection-created') {
-		}
-		if(channel === 'collection-updated') {
-		}
-		if(channel === 'collection-deleted') {
+		switch(channel){
+            case 'game-created': gameCreated(dataJson.data); break;
 		}
 	}
 });
+
+function gameCreated(data)
+{
+    let user = onlineUsers.find(val => val.id === data.user.id);
+    if(!user){
+        return;
+    }
+
+    io.sockets.connected[user.socketId].join(`game::${data.game.id}`);
+}
 
 io.use(function(socket, next) {
     let token = socket.handshake.query && socket.handshake.query.accessToken;
@@ -22,11 +28,10 @@ io.use(function(socket, next) {
         next(new Error('Authentication error'));
     }
     var options = {
-        uri: `${process.env.REST_CLIENT}me`,
+        uri: `${process.env.REST_CLIENT}me?api_token=${token}`,
         headers: {
             'User-Agent': 'Request-Promise',
             'Accept': 'application/json',
-            'Authorization': `Bearer ${token}`
         },
         rejectUnauthorized: false,
         json: true // Automatically parses the JSON string in the response
@@ -34,33 +39,30 @@ io.use(function(socket, next) {
     rp(options)
         .then((res) => {
             socket.user = res;
+            socket.user.socketId = socket.id;
             next();
         })
         .catch((err) => {
-            console.log('Handshake query rejected', err);
+            console.log('Handshake query rejected');
             next(new Error(err));
         });
 });
 
 io.sockets.on('connection', (socket) => {
-    var collections = socket.user.collection_ids;
-    console.log(socket.user.id);
     // collections = collections.map((val) => val.id);
-    socket.join(`user::${socket.user.id}`);
-    if(socket.user){
-        // Join to room named collection::Id
-        collections.forEach((val) => {
-            socket.join(`collection::${val}`);
-        });
+    socket.join(`user::online`);
 
-    }
+    onlineUsers.push(socket.user);
+
+    io.sockets.in(`user::online`).emit('online-user-list-changed', onlineUsers);
+
     socket.on('disconnect', (data) => {
-        // disconnected from rooms named collection::Id
-        collections.forEach((val) => {
-            console.log(`leaving collection::${val}`)
-            socket.leave(`collection::${val}`);
-        });
-        socket.leave(`user::${socket.user.id}`);
+        socket.leave(`user::online`);
+        let userIndex = onlineUsers.findIndex(val => val.id === socket.user.id);
+        if(userIndex !== -1){
+            onlineUsers.splice(userIndex, 1);
+        }
+        io.sockets.in(`user::online`).emit('online-user-list-changed', onlineUsers);
 
     })
 });
